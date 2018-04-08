1e9bbbf021b5432444088bd10e83a3ccb2dac8d0
DESKTOP-3911841\gyere <gyeresicsaba@gmail.com>
2018-04-08 18:46:47 +0200

    .travis.yml

--- app/src/main/java/com/example/gyere/footballdata/ui/details/DetailsActivity.java
+++ app/src/main/java/com/example/gyere/footballdata/ui/details/DetailsActivity.java
@@ -1,13 +1,15 @@
-package com.example.gyere.footballdata;
+package com.example.gyere.footballdata.ui.details;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.gyere.footballdata.R;
+
+public class DetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_details);
     }
 }
--- app/src/main/java/com/example/gyere/footballdata/ui/main/MainActivity.java
+++ app/src/main/java/com/example/gyere/footballdata/ui/main/MainActivity.java
@@ -1,13 +1,15 @@
-package com.example.gyere.footballdata;
+package com.example.gyere.footballdata.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DetailsActivity extends AppCompatActivity {
+import com.example.gyere.footballdata.R;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
+        setContentView(R.layout.activity_main);
     }
 }
