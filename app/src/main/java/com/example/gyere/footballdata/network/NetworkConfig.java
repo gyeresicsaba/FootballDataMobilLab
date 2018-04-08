36804805dd032dad20c7d3189e25e2a379d51e62
DESKTOP-3911841\gyere <gyeresicsaba@gmail.com>
2018-04-08 19:18:33 +0200

    gradlew fix 4

--- app/src/main/java/com/example/gyere/footballdata/network/NetworkConfig.java
+++ app/src/main/java/com/example/gyere/footballdata/network/NetworkConfig.java
@@ -2,5 +2,5 @@ package com.example.gyere.footballdata.network;
 
 public class NetworkConfig {
     public static final String ENDPOINT_ADDRESS = "http://api.football-data.org/v1/competitions/398/";
-    public static final String AUTH_TOKEN = "my_super_token";
+    public static final String AUTH_TOKEN = "my_super_tokenn";
 }
