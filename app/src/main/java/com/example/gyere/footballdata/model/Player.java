fd49103c18ac926179d277e24d91d5121307777e
DESKTOP-3911841\gyere <gyeresicsaba@gmail.com>
2018-04-08 17:11:27 +0200

    player, team models implementation

--- app/src/main/java/com/example/gyere/footballdata/model/Player.java
+++ app/src/main/java/com/example/gyere/footballdata/model/Player.java
@@ -1,4 +1,23 @@
 package com.example.gyere.footballdata.model;
 
 public class Player {
+    private int id;
+    private String name;
+    private int jerseyNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getJerseyNumber() {
+        return jerseyNumber;
+    }
+
+    public void setJerseyNumber(int jerseyNumber) {
+        this.jerseyNumber = jerseyNumber;
+    }
 }
