4214b0b733c9f8ef7209fa7e8433c430a72b9940
csgyeresi <csgyeresi@mik.bme.hu>
2018-04-21 00:20:40 +0200

    fix

--- app/src/main/java/com/example/gyere/footballdata/FootballDataApplicationComponent.java
+++ app/src/main/java/com/example/gyere/footballdata/FootballDataApplicationComponent.java
@@ -1,9 +1,7 @@
 package com.example.gyere.footballdata;
 
-import com.example.gyere.footballdata.interactor.Team.TeamInteractor;
 import com.example.gyere.footballdata.network.NetworkModule;
 import com.example.gyere.footballdata.ui.UIModule;
-import com.example.gyere.footballdata.ui.details.DetailsActivity;
 import com.example.gyere.footballdata.ui.main.MainActivity;
 
 import javax.inject.Singleton;
@@ -14,8 +12,4 @@ import dagger.Component;
 @Component(modules = {UIModule.class, NetworkModule.class})
 public interface FootballDataApplicationComponent {
     void inject(MainActivity mainActivity);
-    void inject(DetailsActivity detailsActivity);
-    void inject(TeamInteractor teamInteractor);
-
-
 }
